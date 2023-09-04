08a4cac296f4a852f37bb2fe905f0903cb3d6d2d
Medetcan Diler <medetdiler1996@gmail.com>
2023-09-04 16:33:18 +0300

    Initilised the project

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className=''>
+      <div className='h-screen'>sadfasdf</div>
+      <div className='h-screen'>sadfasdf</div>
+    </main>
+  )
+}
