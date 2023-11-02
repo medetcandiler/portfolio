64304e73aa3393e32b5a6135ef347748e9992b98
Medetcan Diler <medetdiler1996@gmail.com>
2023-11-02 16:22:20 +0300

    Updated animations on every section

--- app/components/About/About.js
+++ app/components/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
   return (
     <section ref={aboutRef} >
       <div className={` flex flex-col items-center space-y-6 px-6 md:flex-row md:space-y-0 md:space-x-10 `}>
-        <div id="about" className={`left text-md flex flex-col text-center space-y-5  leading-relaxed md:text-lg md:w-2/3 md:text-start transition-all duration-700 ease-in-out ${isVisible ? 'opacity-1 translate-y-0 md:translate-x-0' : 'opacity-0 -translate-y-5 md:-translate-x-5 md:translate-y-0'} `}>
+        <div id="about" className={`left text-md flex flex-col text-center space-y-5  leading-relaxed md:text-lg md:w-2/3 md:text-start transition-all duration-700 ease-in-out ${isVisible ? 'opacity-1 md:translate-x-0' : 'opacity-0 -translate-y-5 md:-translate-x-5'} `}>
           <h1 className="text-5xl font-semibold " >Hey there! <span className="text-5xl" role="img" aria-label="hello">👋</span></h1>
           <p> I'm a Frontend Developer with a passion for creating captivating web experiences.</p>
           <p>After working as an engineer in the field for several months, I realized that my true calling lay in web development. I've always been drawn to coding, software, and computers, and I decided to make a career change to pursue my passion.
@@ -51,7 +51,7 @@ const About = () => {
               </Link></p>{' '}
           </div>
         </div>
-        <div className={`right flex justify-center md:w-1/3 transition-all duration-300 ease-in-out ${isVisible ? 'opacity-1 translate-y-0 md:translate-x-0' : 'opacity-0 -translate-y-5 md:translate-x-5 md:translate-y-0'}`}>
+        <div className={`right flex justify-center md:w-1/3 transition-all duration-300 ease-in ${isVisible ? 'opacity-1 md:translate-x-0' : 'opacity-0 -translate-y-5 md:translate-x-5'}`}>
           <div className="flex flex-col items-center space-y-6">
             <div className="w-[200px] h-[200px] lg:w-[250px] lg:h-[250px]">
               <Image
--- app/components/HeroSection/HeroSection.js
+++ app/components/HeroSection/HeroSection.js
@@ -26,7 +26,7 @@ const HeroSection = () => {
   return (
     <section ref={heroRef} >
       <div className={`container mx-auto flex h-screen-80 px-6 w-full justify-center items-center space-x-4`}>
-        <div className={`left w-full text-center flex flex-col space-y-3 md:space-y-6 md:w-3/5 md:text-start transition-all duration-300 ease-in ${isVisible ? 'opacity-1 translate-y-0 md:translate-x-0' : 'opacity-0 -translate-y-5 md:-translate-x-5 md:translate-y-0'}`}>
+        <div className={`left w-full text-center flex flex-col space-y-3 md:space-y-6 md:w-3/5 md:text-start transition-all duration-300 ease-in ${isVisible ? 'opacity-1  md:translate-x-0' : 'opacity-0 md:-translate-x-5'}`}>
           <h1 className='text-4xl font-bold md:text-5xl lg:text-6xl xl:text-7xl'>Medetcan Diler</h1>
           <div className=' flex items-center justify-center space-x-2 text-2xl font-medium md:text-2xl md:justify-start lg:text-4xl -mx-6 md:-mx-0 '>
             <p className='whitespace-nowrap'>I am a</p>
