5bc811dbb066961e234dfb5b7e778a4e98930e6c
Medetcan Diler <medetdiler1996@gmail.com>
2023-11-05 19:58:12 +0300

    Fixed languagePicker issues

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,33 +1,29 @@
 'use client'
 import Image from "next/image";
-import { startTransition } from "react";
-import { useState } from "react";
-import { useTranslations } from "next-intl";
+import { useState, startTransition } from "react";
+import { useTranslations, useLocale } from "next-intl";
 
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
-import LanguagePicker from "../LanguagePicker/Picker";
 
 const Navbar = () => {
-  const [selectedLang, setSelectedLang] = useState('en');
+  const locale = useLocale();
+  const [selectedLang, setSelectedLang] = useState(locale);
   const t = useTranslations('Navbar');
-  const scrollToSection = (e) => {
-    const section = e.target.textContent.split(`'`)[0].toLowerCase();
+  const scrollToSection = (section) => {
     if (typeof document !== "undefined") {
       const workSection = document.getElementById(`${section}`);
       if (workSection) {
         workSection.scrollIntoView({ behavior: "smooth" });
       }
     }
-  }
+  };
+  console.log(locale)
+  const handleLocaleChange = (e) => {
+    const newLang = e.target.value;
+    setSelectedLang(newLang);
+    window.location.href = `/${newLang}`
+  };
 
-  const handleLocaleChange = (lang) => {
-    console.log('lang', lang)
-    console.log('selectedLang', selectedLang)
-    setSelectedLang(lang);
-    startTransition(() => {
-      window.location.href = `/${lang}`
-    })
-  }
   return (
     <header className="relative container mx-auto px-8 flex justify-between items-center h-[80px] z-50 md:px-0">
       <div className="left">
@@ -40,14 +36,19 @@ const Navbar = () => {
         />
       </div>
       <div className="hidden right  space-x-8 text-[16px] font-semibold md:flex">
-        <button onClick={e => scrollToSection(e)} className="link">{t('work')}</button>
-        <button onClick={e => scrollToSection(e)} className="link">{t('about')}</button>
-        <button onClick={e => scrollToSection(e)} className="link">{t('lets')}</button>
+        <button onClick={() => scrollToSection('work')} className="link">{t('work')}</button>
+        <button onClick={() => scrollToSection('about')} className="link">{t('about')}</button>
+        <button onClick={() => scrollToSection('let')} className="link">{t('lets')}</button>
       </div>
       <section className="md:hidden">
         <HamburgerMenu />
       </section>
-      <LanguagePicker selectedLang={selectedLang} handleLocaleChange={handleLocaleChange} />
+      <div className="fixed z-50 top-[26px] right-6 md:top-6 md:right-5 lg:right-7 xl:right-16">
+        <select className="bg-transparent" value={selectedLang} onChange={handleLocaleChange} name="picker">
+          <option value="tr">tr</option>
+          <option value="en">en</option>
+        </select>
+      </div>
     </header>
   )
 }
